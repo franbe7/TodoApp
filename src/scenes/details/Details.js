cc37896ce19d1f0f34e64dca02a5ab41b0eac9db
Francisco Betancor <francisco.betancor@xmartlabs.com>
2019-05-21 15:40:31 -0300

    added butons for mark as done a tas in the detail screen

--- src/scenes/details/Details.js
+++ src/scenes/details/Details.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import colors from "src/helpers/Colors";
 import homeStyles from "src/scenes/home/Home.styles";
 import detailStyles from "./Details.styles";
@@ -20,8 +20,20 @@ class Details extends Component {
     return (
       <View style={homeStyles.listContainer}>
         <View style={detailStyles.container}>
+          <TouchableOpacity>
+            <Text style={detailStyles.notDone}>
+              Not Done
+            </Text>
+          </TouchableOpacity>
           <Text style={detailStyles.title}>{task.title}</Text>
           <Text style={detailStyles.description}>{task.description}</Text>
+          <TouchableOpacity>
+            <View style={detailStyles.markAsDone}>
+              <Text style={detailStyles.markAsDone_text}>
+                MARK AS DONE
+              </Text>
+            </View>
+          </TouchableOpacity>
         </View>
       </View>
     );
