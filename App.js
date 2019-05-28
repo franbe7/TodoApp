edf8196c6e941e45a90aed2f3714c89b8732b04a
Francisco Betancor <francisco.betancor@xmartlabs.com>
2019-05-28 11:31:49 -0300

    Redux config

--- App.js
+++ App.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
 import { createAppContainer, createStackNavigator } from "react-navigation";
 
 import Details from "./src/scenes/details/Details";
 import { Home } from "./src/scenes/home/Home";
-import { NewTask } from './src/scenes/newTask/NewTask';
+import { NewTask } from "./src/scenes/newTask/NewTask";
 
 import { Route } from "src/helpers/Route";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
-export default function App() {
-  return <AppContainer />;
+import rootReducer from "src/reducers";
+
+const store = createStore(rootReducer);
+
+export default class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <AppContainer />
+      </Provider>
+    );
+  }
 }
 
 const AppNavigator = createStackNavigator(
@@ -21,7 +33,7 @@ const AppNavigator = createStackNavigator(
     },
     [Route.NewTask]: {
       screen: NewTask
-    } 
+    }
   },
   {
     initialRouteName: Route.Home
--- index.js
+++ index.js
@@ -1,15 +1,5 @@
-import React from "react";
-import { render } from "react-dom";
-import { createStore } from "redux";
-import { Provider } from "react-redux";
-import rootReducer from "./reducers";
+import { AppRegistry } from "react-native";
+import { name as appName } from "./app.json";
 import App from "./App";
 
-const store = createStore(rootReducer);
-
-render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
-);
+AppRegistry.registerComponent(appName, () => App);
